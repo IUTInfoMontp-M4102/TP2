63813ee2d392f6ad74be60e3d48eda6f05072678
Victor Poupet <37943580+vpoupet@users.noreply.github.com>
2022-02-04 15:57:48 +0100

    Changement type Executor -> ExecutorService

--- src/Ex2Main.java
+++ src/Ex2Main.java
@@ -2,8 +2,8 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Executor;
 
 public class Ex2Main {
-    public static void main(String [] args) { Executor es;
-        es = Executors.newSingleThreadExecutor();
+    public static void main(String [] args) {
+        ExecutorService es = Executors.newSingleThreadExecutor();
 
         System.out.println(" debut tache principale ");
         es.execute(new Ex2Tache(1, 100, 10));
